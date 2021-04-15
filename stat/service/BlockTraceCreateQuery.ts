233fd1089b5d3021ca6f725aa39ad259052e2586
zhenting <zhenting@confluxnetwork.org>
2021-04-15 16:29:52 +0800

    FIX: add tip for no create trace

--- stat/service/BlockTraceCreateQuery.ts
+++ stat/service/BlockTraceCreateQuery.ts
@@ -9,13 +9,13 @@ export class BlockTraceCreateQuery{
         const contractAddrHex: Hex40Map = await Hex40Map.findOne({where: {hex: simpleAddr.substr(2)}});
         if(contractAddrHex === null){
             console.error(`getTraceCreate no contract ${contractAddr}`);
-            return {msg: `no contract`};
+            return {msg: `get create trace, no contract ${contractAddr} found`};
         }
 
         const traceCreate:TraceCreateContract = await TraceCreateContract.findOne({where: {to: contractAddrHex.id}});
         if(traceCreate === null){
             console.error(`getTraceCreate no trace_create_contract for contract ${contractAddr}`);
-            return {msg: `no create trace`};
+            return {msg: `get create trace, no create trace found for contract ${contractAddr}`};
         }
 
         const txHashHex: Hex64Map = await Hex64Map.findOne({where: {id: traceCreate.txHashId}});
