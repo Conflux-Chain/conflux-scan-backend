404315756ad9414a7a2356467f2b24a2423296b6
Tigo9527 <68840946+Tigo9527@users.noreply.github.com>
2021-04-29 18:52:55 +0800

    0429/daily stat 2 (#40)
    * Schedule daily token stat.
    * Fix daily token stat. table name.
    * Log switcher.

--- stat/service/tool/FixDailyTokenStat.ts
+++ stat/service/tool/FixDailyTokenStat.ts
@@ -2,26 +2,39 @@ import {calcDailyActiveAddress, DailyActiveAddress} from "../../model/StatAddres
 
 import {loadConfig} from "../../config/StatConfig";
 import {createDB, initModel} from "../DBProvider";
-import {DailyTxnSync} from "../DailyTxnSync";
+import {calcAllRegisteredTokenDailyStat, calcDailyToken, DailyTxnSync} from "../DailyTxnSync";
 async function init() {
     const config = loadConfig('Prod')
     let seq = createDB(config.database)
     await seq.sync({})
     await initModel(seq)
 }
-export async function fixDate() {
+export async function fixDate(hexId=0) {
     let dt = new Date('2020-10-28')
     let now = new Date()
     while( dt < now) {
-        await DailyTxnSync.calcAllRegisteredTokenDailyStat(dt)
-        dt = new Date(dt.getTime() + 1000*3600*24)
+        if (hexId) {
+            await calcDailyToken(dt, hexId)
+        } else {
+            await calcAllRegisteredTokenDailyStat(dt)
+        }
+        dt.setDate(dt.getDate()+1)
     }
     console.log(`done.`)
 }
 
 if (require.main === module) {
+    const args = process.argv.slice(2)
     init().then(()=>{
-        return fixDate()
+        if (args.length === 1) {
+            // node this 123
+            return fixDate(Number(args[0]))
+        } else if (args[0]) {
+            // node this '2021-04-29' 123
+            return calcDailyToken(new Date(args[0]), Number(args[1]))
+        } else {
+            return fixDate()
+        }
     }).then(()=>{
         DailyActiveAddress.sequelize.close().then()
     })
