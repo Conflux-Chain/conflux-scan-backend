d9c2f1021aa05300237bc122d506589ec80b05a5
zhenting <zhenting@confluxnetwork.org>
2021-06-01 11:30:24 +0800

    ADD: query token which not registered

--- stat/model/Epoch.sql
+++ stat/model/Epoch.sql
@@ -1,12 +1,11 @@
 CREATE TABLE `epoch` (
-  `id` bigint(20) NOT NULL,
+  `epoch` bigint(20) NOT NULL,
   `timestamp` datetime NOT NULL,
-  `parentHash` bigint(20) NOT NULL,
   `pivotHash` bigint(20) NOT NULL,
-  PRIMARY KEY (`id`),
+  PRIMARY KEY (`epoch`),
   KEY `time_idx` (`timestamp`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4
-partition by range (id) (
+partition by range (epoch) (
     PARTITION p1 VALUES LESS THAN (10000000/*1Kw*/),
     PARTITION p2 VALUES LESS THAN (20000000/*2Kw*/),
     PARTITION p3 VALUES LESS THAN (30000000/*3Kw*/),
