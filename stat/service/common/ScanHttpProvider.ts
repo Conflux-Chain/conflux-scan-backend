c9bc7d50e897e1bd401fc57a4bd96be83dcfff4b
Kang <kangjianhu@conflux-chain.org>
2021-07-29 12:04:46 +0800

    limit to 1 !

--- stat/service/common/ScanHttpProvider.ts
+++ stat/service/common/ScanHttpProvider.ts
@@ -1,5 +1,7 @@
 const HttpProvider = require("js-conflux-sdk/src/provider/HttpProvider")
 const superagent = require('superagent');
+const pLimit = require('p-limit');
+const limit = pLimit(1);
 export class ScanHttpProvider extends HttpProvider {
     tag: string
     times = 0
@@ -9,6 +11,9 @@ export class ScanHttpProvider extends HttpProvider {
         this.tag = tag
     }
     async request(data) {
+        return limit(()=>this.request0(data))
+    }
+    async request0(data) {
         this.times ++
         this.methodTimes[data.method] = (this.methodTimes[data.method] || 0) + 1
         console.log(` ----- ${this.tag}, total times ${this.times}: request rpc ${data.method
