e385aa6e28b12f1638b13003e37cb2507da5a314
Kang <kangjianhu@conflux-chain.org>
2021-08-09 17:59:39 +0800

    custodian token flag in redis.

--- stat/service/tool/TokenTool.ts
+++ stat/service/tool/TokenTool.ts
@@ -2,6 +2,7 @@ import {Conflux} from "js-conflux-sdk";
 import {Token} from "../../model/Token";
 import {init} from "./FixDailyTokenStat";
 import {patchHttpProvider} from "../common/utils";
+import {HASH_CUSTODIAN_TOKEN, RedisWrap} from "../RedisWrap";
 const abi = require('./abi');
 const lodash = require('lodash');
 
@@ -73,12 +74,38 @@ export class TokenTool {
     }
 }
 
-async function updateTotalSupply() {
+async function updateCustodianTokenFlag() {
+    const tool = await initTool()
+    async function repeat() {
+        const list = await Token.findAll({where: {auditResult: true,}});
+        let trueCount = 0
+        for (const token of list) {
+            const is = await tool.contract.totalSupply()
+                .call({to: token.base32}).catch(err => {
+                    console.log(`call proxy contract fail, token ${token.base32}`, err)
+                    return false
+                })
+            trueCount += is ? 1 : 0
+            await RedisWrap.hSet(HASH_CUSTODIAN_TOKEN, token.base32, is ? '1' : '')
+        }
+        setTimeout(repeat, 10_000)
+        console.log(`set to true count ${trueCount}`)
+    }
+    repeat().then()
+}
+
+async function initTool() {
     const cfg = await init()
     const cfx = new Conflux(cfg.conflux)
     console.log(`conflux: `, cfg.conflux)
     patchHttpProvider(cfx, cfg.conflux)
     const tool = new TokenTool(cfx)
+    return tool;
+}
+
+async function updateTotalSupply() {
+    const tool = await initTool();
+
     async function repeat() {
         const list = await Token.findAll({where: {auditResult: true,
                 // symbol:'PHM-NFT'
@@ -110,5 +137,10 @@ async function updateTotalSupply() {
 }
 
 if (module === require.main) {
-    updateTotalSupply().then()
+    const args = process.argv.slice(2)
+    if (args[0] === 'custodian_token') {
+        updateCustodianTokenFlag().then()
+    } else {
+        updateTotalSupply().then()
+    }
 }
