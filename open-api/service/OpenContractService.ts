60d64b3819716a9ac4b7e16114c140ee8df1d81e
Kang <kangjianhu@conflux-chain.org>
2021-08-25 16:59:12 +0800

    remove empty key.

--- open-api/service/OpenContractService.ts
+++ open-api/service/OpenContractService.ts
@@ -17,5 +17,21 @@ export async function polishContract(page) {
         return
     }
     const basicInfo = await getApiService().contractQuery.listBasic({addressArray:[...contract], iconUrl: true})
+    const map = basicInfo.map
+    Object.keys(map).forEach(k=>{
+        delete map[k].contract?.address
+        delete map[k].token?.address
+        removeEmptyKey(map[k], 'contract')
+        removeEmptyKey(map[k], 'token')
+        // removeEmptyKey(map, k)  // keep address, help debugging.
+    })
     page.addressInfo = basicInfo.map
+}
+export function removeEmptyKey(obj, key) {
+    if (isEmptyObj(obj[key])) {
+        delete obj[key]
+    }
+}
+export function isEmptyObj(obj) {
+    return !obj || !Object.keys(obj).map(k=>obj[k]).some(v=> v!==undefined && v!==null)
 }
