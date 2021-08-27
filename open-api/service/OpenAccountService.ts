b7a793c1840ea8ef71ea2f2472895fbcc5f7af67
Kang <kangjianhu@conflux-chain.org>
2021-08-27 13:35:42 +0800

    prefix icon url.

--- open-api/service/OpenAccountService.ts
+++ open-api/service/OpenAccountService.ts
@@ -3,7 +3,16 @@ export function polishAssertList(page) {
         row.amount = row.balance
         row.type = row.type?.replace('ERC', 'CRC')
         row.contract = row.base32
-        delete row.tokenHex40id
+        if (row.iconUrl) {
+            if (!row.iconUrl.startsWith('http://')) { // without prefix
+                if (row.base32.startsWith('cfx:')) { // mainnet
+                    row.iconUrl = 'https://confluxscan.io/'
+                } else if (row.base32.startsWith('cfxtest:')) { // testnet
+                    row.iconUrl = 'https://testnet.confluxscan.io/'
+                }
+            }
+        }
+        delete row.tokenHex40id;
         delete row.balance
         delete row.base32
     })
