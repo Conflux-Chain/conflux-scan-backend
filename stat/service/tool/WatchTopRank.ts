f7b8c4fb6ddd37b9bbcc645a9fadd00cade9d900
Kang <kangjianhu@conflux-chain.org>
2021-09-06 17:14:23 +0800

    WatchTopRank.ts

---
+++ stat/service/tool/WatchTopRank.ts
@@ -0,0 +1,26 @@
+import {CfxBalance} from "../../model/Balance";
+import {CfxWatcher} from "../watcher/BalanceWatcher";
+import {Hex40Map} from "../../model/HexMap";
+import {init} from "./FixDailyTokenStat";
+import {Conflux} from "js-conflux-sdk";
+
+async function watchCfx(col, watcher: CfxWatcher) {
+    const byBal = await CfxBalance.findAll({
+        order: [[col, 'desc']], limit: 500
+    })
+    for (const row of byBal) {
+        const hex = await Hex40Map.findByPk(row.addressId)
+        await watcher.queryBalance(`0x${hex}`, row.addressId)
+    }
+}
+async function fixCfx(watcher: CfxWatcher) {
+    await watchCfx('balance', watcher)
+    await watchCfx('stakingBalance', watcher)
+    await watchCfx('total', watcher)
+}
+
+init().then(config=>{
+    const cfx = new Conflux(config.conflux)
+    const w = new CfxWatcher('cfx', cfx);
+    return fixCfx(w);
+})
