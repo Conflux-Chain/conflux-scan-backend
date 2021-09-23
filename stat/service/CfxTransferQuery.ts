26b6168d3ce08019612368b77ead1d7929b8ecce
Kang <kangjianhu@conflux-chain.org>
2021-09-23 17:48:35 +0800

    sort.

--- stat/service/CfxTransferQuery.ts
+++ stat/service/CfxTransferQuery.ts
@@ -20,7 +20,7 @@ export class CfxTransferQuery extends TransferQueryBase{
     public buildQueryOptions({minEpochNumber, maxEpochNumber, transactionHashId,
                                  minTimestamp, maxTimestamp,
                                  accountAddressId, addressId, fromAddressId, toAddressId, opponentAddressId, tokenAddressIdArray,
-                                 tokenId, txType, skip, limit}){
+                                 tokenId, txType, skip, limit, sort}){
         const{ logger } = this.app;
         if(txType === CONST.TX_TYPE.CREATE){
             // page
@@ -55,14 +55,14 @@ export class CfxTransferQuery extends TransferQueryBase{
                 queryOptions.where[Op.and] = conditionArray;
             }
             // order
-            queryOptions.order = [['epochNumber', 'DESC'],['id', 'DESC']];
+            queryOptions.order = [['epochNumber', sort],['id', sort]];
             return queryOptions;
         }
 
        return super.buildQueryOptions({minEpochNumber, maxEpochNumber, transactionHashId,
             minTimestamp, maxTimestamp,
             accountAddressId, addressId, fromAddressId, toAddressId, opponentAddressId, tokenAddressIdArray,
-            tokenId, txType, skip, limit});
+            tokenId, txType, skip, limit, sort});
     }
 
     public buildQueryFields({accountAddressId, txType}): any{
