40d2623b11ab5f93973d3c11fee4e528b95b65b7
Zhenting Wang <argeric@qq.com>
2021-12-10 18:22:35 +0800

    1210/conflux context internal contract (#248)
    * ADD: ConfluxContext contract
    
    Co-authored-by: zhenting <zhenting@confluxnetwork.org>

---
+++ stat/service/abi/ConfluxContext.ts
@@ -0,0 +1,41 @@
+export const abi = [
+  {
+    "inputs": [],
+    "name": "epochNumber",
+    "outputs": [
+      {
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [],
+    "name": "posHeight",
+    "outputs": [
+      {
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [],
+    "name": "finalizedEpochNumber",
+    "outputs": [
+      {
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  }
+]
