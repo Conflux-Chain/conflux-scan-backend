082f909d96da53ff227e9344c04eeed80a69df88
Argeric <argeric@qq.com>
2022-01-25 11:55:10 +0800

    UPDATE: add catch code when get epoch number in sync base (#264)
    Co-authored-by: zhenting <zhenting@confluxnetwork.org>

--- stat/service/SyncBase.ts
+++ stat/service/SyncBase.ts
@@ -128,12 +128,19 @@ export abstract class SyncBase{
         }
 
         const that = this
+        let stateEpochNumber = await cfx.getEpochNumber(CONST.EPOCH_NUMBER.LATEST_STATE).catch(e => {
+            console.log(` SyncBase getEpochNumber error:${e}`);
+            return 0;
+        });
         async function repeat() {
-            const stateEpochNumber = await cfx.getEpochNumber(CONST.EPOCH_NUMBER.LATEST_STATE);
             if (traceEpochNumber <= stateEpochNumber - (config.preload)) {
                 traceEpochNumber = await that.syncForward(traceEpochNumber);
                 setTimeout(repeat, 0)
             } else {
+                stateEpochNumber = await cfx.getEpochNumber(CONST.EPOCH_NUMBER.LATEST_STATE).catch(e => {
+                    console.log(` SyncBase getEpochNumber error:${e}`);
+                    return 0;
+                });
                 setTimeout(repeat, 1000)
             }
         }
