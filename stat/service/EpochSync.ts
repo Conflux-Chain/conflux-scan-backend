b3913fc522f6f7e55f3009d6ff843dd24582c404
Kang <kangjianhu@conflux-chain.org>
2022-02-07 11:07:38 +0800

    rollback temp code for evm

--- stat/service/EpochSync.ts
+++ stat/service/EpochSync.ts
@@ -511,7 +511,8 @@ export class EpochSync extends SyncBase{
 
     private async getTraceArray(epochNumber) {
         let traceArray = [];
-        if (process.env.noTrace || true) {
+        if (process.env.noTrace) {
+            // evm space has no trace for now.
             return traceArray;
         }
         const [blockArray, traceArray2d] = await this.getBlockArray(epochNumber);
