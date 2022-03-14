13a695ea50bc31acf193d8ebf8259f4ae9eb5e15
Kang <kangjianhu@conflux-chain.org>
2022-03-14 11:47:24 +0800

    ding msg

--- stat/service/watcher/DummyNode.ts
+++ stat/service/watcher/DummyNode.ts
@@ -201,7 +201,7 @@ export class DummyNode {
                     const {action, type, valid, markCallResult} = trace;
                     if (!valid || markCallResult !=='success') {
                         const msg = `Cfx history: Trace is valid ? [${valid
-                        }], markCallResult [${markCallResult}]. epoch ${epoch} tx ${transactionHash}`;
+                        }], markCallResult [${markCallResult}]. epoch ${epoch} tx ${transactionHash}.\n [scan]`;
                         console.log(`${msg}`);
                         console.log(`traces: ${JSON.stringify(traces)}`)
                         if (this.dingToken){
