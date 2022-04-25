093c224b6c82303992661743c2f3f7b26d0d8852
Kang <kangjianhu@conflux-chain.org>
2022-04-25 10:34:25 +0800

    check nft mint.

---
+++ stat/service/tool/NftOwnerCheck.ts
@@ -0,0 +1,67 @@
+import {Erc1155Data, NftMint, Token} from "../../model/Token";
+import {QueryTypes} from "sequelize";
+import {Conflux} from "js-conflux-sdk";
+import {Hex40Map} from "../../model/HexMap";
+
+const {abi: abi1155} = require('../watcher/contract/miniERC1155.json')
+async function checkNftMint(contractId:number) {
+    const token = await Token.findOne({attributes:{exclude: ['icon']},where:{hex40id: contractId,
+            // type: 'ERC1155'
+    }})
+    if (token.type !== 'ERC1155') {
+        console.log(`not 1155: ${token.type}`)
+        process.exit(9)
+    }
+    nftContract = cfx.Contract({abi: abi1155, address: token.base32});
+    const mintList = await NftMint.findAll({
+        where: {contractId}
+    })
+    const dataList = await Erc1155Data.findAll({
+        where: {contractId}
+    })
+    console.log(`mint count ${mintList.length}, computed from transfer ${dataList.length}`)
+    //
+    const dataMap = new Map<any, Erc1155Data>()
+    dataList.forEach(d=>dataMap.set(d.tokenId, d))
+    for (let i = 0; i < mintList.length; i++) {
+        const mint = mintList[i]
+        const data = dataMap.get(mint.tokenId)
+        if (!data) {
+            console.log(`not found in erc1155data. token ${mint.tokenId}`)
+            process.exit(9)
+        }
+        if (data.addressId === mint.toId) {
+            console.log(`match ${mint.tokenId}`)
+            continue
+        }
+        const hex = await Hex40Map.findByPk(data.addressId).then(res=>'0x'+res.hex)
+        if (token.type === 'ERC1155') {
+            const balance = await nftContract.balanceOf(hex, BigInt(data.tokenId))
+            console.log(`check balance, ${hex} holds ${data.tokenId} x ${balance}`)
+            if (balance < 0) {
+                console.log(`need fix`)
+                process.exit(8)
+            }
+        } else {
+
+        }
+    }
+}
+let nftContract;
+let cfx;
+async function main() {
+    const [,,cmd,cfxUrl,contractId] = process.argv
+    cfx = new Conflux({url: cfxUrl})
+    if (cmd === 'checkNftMint') {
+        await checkNftMint(parseInt(contractId))
+        console.log(`done`)
+        await NftMint.sequelize.close()
+        return
+    } else {
+        console.log(`unknown command [${cmd}]`)
+    }
+}
+
+if (module === require.main) {
+    main().then()
+}
