95ab3fa0a5455df4ae17cf45ab9feb74c426cc2a
Kang <kangjianhu@conflux-chain.org>
2022-05-07 14:49:08 +0800

    calcDailyCfxFromEvm

--- stat/service/CrossSpaceStat.ts
+++ stat/service/CrossSpaceStat.ts
@@ -3,8 +3,9 @@ import {Sequelize, fn, col, Op, QueryTypes, Model, DataTypes} from 'sequelize'
 import {Conflux, Drip} from "js-conflux-sdk";
 import {init} from "./tool/FixDailyTokenStat";
 import {makeIdV} from "../model/HexMap";
+import {FullTransaction} from "../model/FullBlock";
 
-export declare type CrossSpaceStat_BIZ = 'DailyCfxToEVM'
+export declare type CrossSpaceStat_BIZ = 'DailyCfxToEVM' | 'DailyCfxFromEVM'
 export interface ICrossSpaceStat {
     id?:number; day:Date; v:number; biz: CrossSpaceStat_BIZ
 }
@@ -24,7 +25,26 @@ export class CrossSpaceStat extends Model<ICrossSpaceStat> implements ICrossSpac
         })
     }
 }
-async function calcDailyCfxToEvm(dt: Date) {
+export async function calcDailyCfxFromEvm(dt: Date) {
+    const evm = chainId == 8888 ? 'eth8889' : 'evm'
+    const [cfx_transfer_2, full_tx] = [CfxTransfer.getTableName(), FullTransaction.getTableName()]
+    // const sql = `select x.fromId, x.toId, x.value,x.type, tx.hash, tx.gasPrice
+
+    const sql = `select sum(x.value) as amt
+    from ${evm}.${cfx_transfer_2} x left join ${evm}.${full_tx} tx 
+    on tx.epoch=x.epoch and tx.blockPosition=x.blockIndex and tx.txPosition=x.txIndex
+    where x.createdAt between ? and ? and tx.toId=${crossSpaceContractId} and tx.gasPrice=0`
+
+    const sumV = await CfxTransfer.sequelize.query(sql, {type: QueryTypes.SELECT, raw: true,
+        logging: console.log, benchmark: true,
+    }).then(res=>res[0]['amt'])
+
+    const [bean] = await CrossSpaceStat.upsert({
+        biz: "DailyCfxFromEVM", v: parseFloat(new Drip(sumV).toCFX()), day: dt,
+    })
+    console.log(`DailyCfxFromEVM ${dt.toISOString()} ${bean?.v}`)
+}
+export async function calcDailyCfxToEvm(dt: Date) {
     const dayStart = new Date(dt.getFullYear(), dt.getMonth(), dt.getDate());
     const dayEnd = new Date(dt.getFullYear(), dt.getMonth(), dt.getDate(), 23, 59, 59)
 
@@ -37,15 +57,17 @@ async function calcDailyCfxToEvm(dt: Date) {
     })
     console.log(`DailyCfxToEVM ${dt.toISOString()} ${bean?.v}`)
 }
-let crossSpaceContractId = 0
 
+let crossSpaceContractId = 0
+let chainId = 0
 async function main() {
     const cfg = await init()
     const cfx = new Conflux(cfg.conflux)
     const st = await cfx.getStatus()
-    console.log(`------- ${st.chainId} ------ crossSpaceContractId ${crossSpaceContractId}`)
+    chainId = st.chainId
     const [,,cmd] = process.argv
     crossSpaceContractId = await makeIdV('0x0888000000000000000000000000000000000006')
+    console.log(`------- net ${st.chainId} ------ crossSpaceContractId ${crossSpaceContractId}`)
     if (cmd === 'calcDailyCfxToEvm') {
         // node stat/dist/service/CrossSpaceStat.js calcDailyCfxToEvm
         let dt = new Date('2022-02-21')
@@ -53,6 +75,15 @@ async function main() {
             await calcDailyCfxToEvm(dt)
             dt.setDate(dt.getDate() + 1)
         }
+    } else if (cmd === 'calcDailyCfxFromEvm') {
+        // node stat/dist/service/CrossSpaceStat.js calcDailyCfxFromEvm
+        let dt = new Date('2022-02-21')
+        while(dt.getTime() < Date.now()) {
+            await calcDailyCfxFromEvm(dt)
+            dt.setDate(dt.getDate() + 1)
+        }
+    } else {
+        console.log(`unknown command [${cmd}]`)
     }
     console.log(`done`)
     process.exit(0)
