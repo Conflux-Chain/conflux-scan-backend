ed2222af685cc3691112ce5f57ed8019a9765622
Kang <kangjianhu@conflux-chain.org>
2022-05-17 17:18:18 +0800

    compilation

--- stat/TimerTask.ts
+++ stat/TimerTask.ts
@@ -49,7 +49,7 @@ async function main() {
     //
     const blockDataStatSync = new DailyBlockDataStatSync();
     await blockDataStatSync.schedule(); // daily block data stat
-    console.log(`Timer tasks scheduled.`)
+    console.log(`----- Timer tasks scheduled. -----`)
 }
 main().then().catch(err=>{
     console.log(`Timer task fail:`, err)
