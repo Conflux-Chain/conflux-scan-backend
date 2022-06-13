fdd8541a532cc92facafecd8dfab7797a9aa7415
Kang <kangjianhu@conflux-chain.org>
2022-06-13 11:41:22 +0800

    trace slow api

--- open-api/test/traceSlowApi.ts
+++ open-api/test/traceSlowApi.ts
@@ -2,7 +2,9 @@ import {init} from "../../stat/service/tool/FixDailyTokenStat";
 import {ApiLog} from "../../stat/monitor/ApiLog";
 import {NFTCheckerService} from "../../stat/service/nftchecker/NFTCheckerService";
 import {StatConfig} from "../../stat/config/StatConfig";
-import {Conflux} from "js-conflux-sdk";
+import {Conflux, format} from "js-conflux-sdk";
+import {getAddrId, Hex40Map} from "../../stat/model/HexMap";
+import {TokenBalance} from "../../stat/model/Balance";
 
 let cfg:StatConfig
 async function main() {
@@ -26,13 +28,24 @@ function parseQueryParam(query: string) {
 }
 
 async function testNftTokens(query:string) {
+    //
     const param = parseQueryParam(query);
+    // holders
+    const cid = await getAddrId(param['contract'])
+    const holderIdList = await TokenBalance.findAll({
+        where: {contractId: cid}, limit: 50,
+    })
     let cfx = new Conflux(cfg.conflux)
     const svc = new NFTCheckerService({cfx})
+    let i = 0
     async function repeat() {
+        const {hex} = await Hex40Map.findOne({where: {id: holderIdList[i].addressId}})
+        param['owner'] = format.address(`0x${hex}`, 1029)
+        console.log('use owner', param['owner'])
         const start = Date.now()
         const result = await svc.getNftTokensForOpenApi(param as any)
         console.log(`${new Date().toISOString()} costs ${Date.now() - start} ms, total ${result.total}`)
+        await repeat() // will hit mysql cache ?
         setTimeout(repeat, 5_000)
     }
     return repeat()
