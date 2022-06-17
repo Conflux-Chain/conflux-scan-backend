382c02ea0dcad2d86e0d02782ca6e49c50be959a
voidsolo <argeric@qq.com>
2022-06-17 14:26:39 +0800

    UPDATE: optimize list tx (#412)
    Co-authored-by: zhenting <zhenting@confluxnetwork.org>

--- stat/service/FullBlockQuery.ts
+++ stat/service/FullBlockQuery.ts
@@ -21,12 +21,14 @@ const CONST = require('./common/constant');
 
 export class FullBlockQuery {
     protected app;
+    protected sdk;
     protected sponsorContract;
     protected recommendGasPrice = BigInt(0);
 
     public constructor(app: any) {
         this.app = app;
-        this.sponsorContract = this.app.cfx.InternalContract('SponsorWhitelistControl');
+        this.sdk = app.cfxSDK || app.cfx;
+        this.sponsorContract = this.sdk.InternalContract('SponsorWhitelistControl');
     }
 
     public async listBlock({minEpochNumber = undefined, maxEpochNumber = undefined, blockHash = undefined,
@@ -454,10 +456,6 @@ export class FullBlockQuery {
     }
 
     public async batchGetTransactionList({hashArray}): Promise<any> {
-        const {
-            app: {cfx}
-        } = this;
-
         const txMap = {};
         const receiptMap = {};
         let total = hashArray?.length;
@@ -471,11 +469,6 @@ export class FullBlockQuery {
         do {
             const txHashArray = hashArray.slice(skip, skip + pageSize)
             if (txHashArray?.length) {
-
-                // const txTaskArray = txHashArray.map(txHash => cfx.getTransactionByHash(txHash));
-                // const receiptTaskArray = txHashArray.map(txHash => cfx.getTransactionReceipt(txHash));
-                // const txArray: any[] = await Promise.all(txTaskArray);
-                // const receiptArray: any[] = await Promise.all(receiptTaskArray);
                 const {txArray, receiptArray} = await this.batchGetTransactionList0({hashArray: txHashArray});
 
                 txArray?.forEach(item => item && (txMap[item.hash] = item));
@@ -487,15 +480,11 @@ export class FullBlockQuery {
     }
 
     public async batchGetTransactionList0({hashArray}): Promise<any> {
-        const {
-            app: {cfx}
-        } = this;
-
         const rpcTxs = hashArray.map(hash=>{return {"method": "cfx_getTransactionByHash","params": [hash]}});
         const rpcReceipts = hashArray.map(hash=>{return {"method": "cfx_getTransactionReceipt","params": [hash]}});
         const rpcBoth = [...rpcTxs, ...rpcReceipts];
         const len = hashArray.length;
-        return cfx.provider.batch(rpcBoth).then(arr=>{
+        return this.sdk.provider.batch(rpcBoth).then(arr=>{
             const txArray = arr.slice(0, len);
             const receiptArray = arr.slice(len);
             return {txArray, receiptArray};
@@ -503,12 +492,8 @@ export class FullBlockQuery {
     }
 
     public async listPendingTx({accountAddress}){
-        const {
-            app: {cfx}
-        } = this;
-
         // check
-        const result =  await cfx.getAccountPendingTransactions(accountAddress, undefined, 10);
+        const result =  await this.sdk.getAccountPendingTransactions(accountAddress, undefined, 10);
         const {firstTxStatus, pendingTransactions} = result;
         if(!pendingTransactions?.length){
             return result;
@@ -521,7 +506,7 @@ export class FullBlockQuery {
             const pendingDetail = {
                 code: 11,
                 message: 'The nonce in [stateNonce, txNonce) is skipped',
-                params:{txNonce: nonce, stateNonce: await cfx.getNextNonce(from)}
+                params:{txNonce: nonce, stateNonce: await this.sdk.getNextNonce(from)}
             };
             lodash.defaults(result, {pendingDetail});
             return result;
@@ -531,7 +516,7 @@ export class FullBlockQuery {
         if(pending?.endsWith('Cash')){
             const gasFee = gas * gasPrice;
             const colFee = storageLimit * BigInt(10**18) / BigInt(1024);
-            const {balance}  = await cfx.getAccount(from);
+            const {balance}  = await this.sdk.getAccount(from);
             const totalCost = value + gasFee + colFee;
             const pendingDetail = {
                 message: 'The balance is insufficient to pay value + gas * gasPrice + storageLimit * (10^18/1024)',
@@ -546,7 +531,7 @@ export class FullBlockQuery {
             }
 
             // EOA
-            const {codeHash}  = await cfx.getAccount(to);
+            const {codeHash}  = await this.sdk.getAccount(to);
             const isEOA = codeHash === '0xc5d2460186f7233c927e7db2dcc703c0e500b653ca82273b7bfad8045d85a470';
             if(isEOA && balance < totalCost){
                 lodash.defaults(result, {pendingDetail: lodash.assign(pendingDetail, {code: 22})});
@@ -554,7 +539,7 @@ export class FullBlockQuery {
             }
 
             // contract
-            const sponsorInfo = await cfx.getSponsorInfo(to);
+            const sponsorInfo = await this.sdk.getSponsorInfo(to);
             const isWhitelisted = await this.sponsorContract.isWhitelisted(to, from);
             const {sponsorForGas, sponsorForCollateral, sponsorGasBound, sponsorBalanceForGas,
                 sponsorBalanceForCollateral} = sponsorInfo;
@@ -577,7 +562,7 @@ export class FullBlockQuery {
         // ready
         if(firstTxStatus?.endsWith('ready')){
             const proposedEpoch = epochHeight;
-            const confirmedEpoch = BigInt(await cfx.getEpochNumber(SDK_CONST.EPOCH_NUMBER.LATEST_CONFIRMED));
+            const confirmedEpoch = BigInt(await this.sdk.getEpochNumber(SDK_CONST.EPOCH_NUMBER.LATEST_CONFIRMED));
             const epochGap = Math.abs(Number(proposedEpoch - confirmedEpoch));
             if(epochGap > 100_000){
                 const pendingDetail = {
