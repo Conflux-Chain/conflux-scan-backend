27f09fa2054ec769d624eadbfe86f949e4bf0ed5
Tigo9527 <68840946+Tigo9527@users.noreply.github.com>
2022-08-17 14:13:54 +0800

    0817/nginx (#449)
    * nginx controller
    
    * bash
    
    * bash2
    
    * server-info
    
    * comment.
    
    Co-authored-by: Kang <kangjianhu@conflux-chain.org>

---
+++ stat/config/gen-nginx-conf.js
@@ -0,0 +1,60 @@
+const entries = [
+    {        name: 'stat_core',     app_port: 8087, nginx_port: 28087,  path:'/stat/server-info'  },
+    {        name: 'scan_api_core', app_port: 8895, nginx_port: 28895, path:'/v1' },
+    {        name: 'open_api_core', app_port: 9527, nginx_port: 29527,  path: '/open'    },
+
+    {        name: 'stat_evm',      app_port: 7087, nginx_port: 37087, path:'/stat/server-info'    },
+    {        name: 'scan_api_evm',  app_port: 7895, nginx_port: 37895,  path:'/v1'  },
+    {        name: 'open_api_evm',  app_port: 19527, nginx_port: 39527, path:'/open'   },
+]
+//
+async function main()
+{
+    const [,,ip='',app='', write, reload] = process.argv
+    console.log(`got ip [${ip}] app [${app}]`)
+
+    const targetApps = entries.filter(e=>e.name === app);
+    if (!ip.match(/^[\d]+\.[\d]+\.[\d]+\.[\d]+$/) || targetApps.length < 1) {
+        console.log(`Usage: node ${__filename} <0.0.0.0> [${entries.map(a=>a.name).join(' | ')}] [writeFlag] [reloadFlag]`)
+        process.exit(9)
+    }
+    const conf = targetApps.map(e=>`
+    # ${e.name}
+    server { 
+            listen ${e.nginx_port};
+            listen [::]:${e.nginx_port};
+            access_log /var/log/nginx/access_${e.name}.log;
+            error_log /var/log/nginx/error_${e.name}.log;
+            location / {
+                    proxy_pass http://${ip}:${e.app_port};
+                    proxy_set_header Connection ""; # clear client side keepalive option.
+           }
+    }
+    `).join('\n');
+    console.log(conf)
+
+    if (write) {
+        const fs = require('fs')
+        fs.writeFileSync(`/etc/nginx/conf.d/${app}.conf`, conf)
+    }
+
+    if (reload) {
+        await bash('nginx -s reload')
+        await new Promise(r=>setTimeout(r, 2000))
+        const [app_] = targetApps
+        await bash(`curl -s 127.0.0.1:${app_.nginx_port}${app_.path}`)
+    }
+}
+const util = require('util');
+const exec = util.promisify(require('child_process').exec);
+async function bash(cmd) {
+    console.log(`now execute: ${cmd}`)
+    try {
+        const { stdout, stderr } = await exec(cmd);
+        console.log('stdout:', stdout);
+        console.log('stderr:', stderr);
+    } catch (err) {
+        console.error(err);
+    }
+}
+main().then()
---
+++ stat/config/nginx.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+# It's a template file, copy it to repository root, change APP and BACKUP_NODE before executing.
+# Possible apps defined in js file :
+# stat_core | scan_api_core | open_api_core | stat_evm | scan_api_evm | open_api_evm
+APP='scan_api_core'
+BACKUP_NODE='172.31.124.19'
+LOCAL='127.0.0.1'
+# override APP by the 2nd parameter
+if [ "$2" != "" ]
+then
+  APP=$2
+fi
+if [ "$1" = 'up' ]
+then
+  IP=$LOCAL
+elif [ "$1" = 'down' ]
+then
+  IP=$BACKUP_NODE
+else
+  echo "Usage: this [ up | down]"
+  exit
+fi
+echo "$1 , use ip [${IP}]"
+node ./stat/config/gen-nginx-conf.js $IP $APP WRITE RELOAD
