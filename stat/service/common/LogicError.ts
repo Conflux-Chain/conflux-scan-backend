d08e938671ffe283301ccfa3282f9acf8f36eb62
zhenting <zhenting@confluxnetwork.org>
2022-09-23 16:55:23 +0800

    UPDATE: refine nft error detail

--- stat/service/common/LogicError.ts
+++ stat/service/common/LogicError.ts
@@ -21,42 +21,42 @@ class LogicError extends Error {
 
 export const Errors = {
     // common error
-    BizError: LogicError.extend({ code: 50100, name: 'BizError' }),
-    ParameterError: LogicError.extend({ code: 50101, name: 'ParameterError' }),
-    PermissionError: LogicError.extend({ code: 50102, name: 'PermissionError' }),
-    ApiBusyError: LogicError.extend({ code: 50103, name: 'ApiBusyError' }),
+    BizError: LogicError.extend({ code: 50100, name: 'Unknown error, please try again later, or submit a ticket' }),
+    ParameterError: LogicError.extend({ code: 50101, name: 'The parameter is wrong, please confirm it is correct' }),
+    PermissionError: LogicError.extend({ code: 50102, name: 'Permission Error' }),
+    ApiBusyError: LogicError.extend({ code: 50103, name: 'API is Busy, try again later' }),
 
     // network error
-    NetworkError: LogicError.extend({ code: 50200, name: 'NetworkError'}),
-    RequestTimeout: LogicError.extend({ code: 50201, name: 'RequestTimeout'}),
-    ResponseDataParsingError: LogicError.extend({ code: 50202, name: 'ResponseDataParsingError'}),
+    NetworkError: LogicError.extend({ code: 50200, name: 'Network exception, try again later'}),
+    RequestTimeout: LogicError.extend({ code: 50201, name: 'External data request timeout, try again later'}),
+    ResponseDataParsingError: LogicError.extend({ code: 50202, name: 'Data parsing exception, try again later'}),
 
     // RPC error
-    RPCCallError: LogicError.extend({ code: 50300, name: 'RPCCallError'}),
-    SendAnnounceError: LogicError.extend({ code: 50301, name: 'SendAnnounceError' }),
-    AnnouncementNotExistError: LogicError.extend({ code: 50302, name: 'AnnouncementNotExistError' }),
-    AnnounceTooLongError: LogicError.extend({ code: 50303, name: 'AnnounceTooLongError' }),
+    RPCCallError: LogicError.extend({ code: 50300, name: 'RPC Call Error, try again later, or submit a ticket'}),
+    SendAnnounceError: LogicError.extend({ code: 50301, name: 'Send Announce Error' }),
+    AnnouncementNotExistError: LogicError.extend({ code: 50302, name: 'Announcement Not Exist Error' }),
+    AnnounceTooLongError: LogicError.extend({ code: 50303, name: 'Announce Too Long Error' }),
 
     // contract error
-    QueryContractError: LogicError.extend({ code: 50400, name: 'QueryContractError' }),
-    QueryCreationDataError: LogicError.extend({ code: 50401, name: 'QueryCreationDataError' }),
+    QueryContractError: LogicError.extend({ code: 50400, name: 'Query Contract Error, try again later, or submit a ticket' }),
+    QueryCreationDataError: LogicError.extend({ code: 50401, name: 'Query Contract Error, data is not synced, try again later' }),
 
     // token error
-    QueryTokenError: LogicError.extend({ code: 50500, name: 'QueryTokenError' }),
-    NotTokenError: LogicError.extend({ code: 50501, name: 'NotTokenError'}),
+    QueryTokenError: LogicError.extend({ code: 50500, name: 'Query Token Error, try again later, or submit a ticket' }),
+    NotTokenError: LogicError.extend({ code: 50501, name: 'Contract Error, does not meet the token standard'}),
 
     // NFT error
-    QueryNFTError: LogicError.extend({ code: 50600, name: 'QueryNFTError' }),
-    CallNFTContractError: LogicError.extend({ code: 50601, name: 'CallNFTContractError' }),
-    QueryNFTMetadataError: LogicError.extend({ code: 50602, name: 'QueryNFTMetadataError' }),
-    ParseNFTMetadataError: LogicError.extend({ code: 50603, name: 'ParseNFTMetadataError' }),
-    QueryNFTLocalNameError: LogicError.extend({ code: 50604, name: 'QueryNFTLocalNameError' }),
-    MetadataPropertyError: LogicError.extend({ code: 50605, name: 'MetadataPropertyError'}),
+    QueryNFTError: LogicError.extend({ code: 50600, name: 'Query NFT Error, try again later, or submit a ticket' }),
+    CallNFTContractError: LogicError.extend({ code: 50601, name: 'Contract Error, failed to get TokenURI from contract' }),
+    QueryNFTMetadataError: LogicError.extend({ code: 50602, name: 'Metadata Error, unable to request the TokenURI returned by the contract' }),
+    ParseNFTMetadataError: LogicError.extend({ code: 50603, name: 'Metadata Error, the metadata for this NFT could not be parsed' }),
+    QueryNFTLocalNameError: LogicError.extend({ code: 50604, name: 'Contract Error, Could not get the Local Name for this NFT' }),
+    MetadataPropertyError: LogicError.extend({ code: 50605, name: 'Metadata Error, the Metadata for this NFT is malformed'}),
 
     // verify error
-    ContractVerifyError: LogicError.extend({ code: 50700, name: 'ContractVerifyError' }),
-    CompilerError: LogicError.extend({ code: 50701, name: 'CompilerError' }),
-    ContractDecompileError: LogicError.extend({ code: 50702, name: 'ContractDecompileError' }),
-    ExtractMetadataError: LogicError.extend({ code: 50703, name: 'ExtractMetadataError' }),
-    ContractNameError: LogicError.extend({ code: 50704, name: 'ContractNameError' }),
+    ContractVerifyError: LogicError.extend({ code: 50700, name: 'Contract Verify Error, try again later, or submit a ticket' }),
+    CompilerError: LogicError.extend({ code: 50701, name: 'Contract Verify Error, error compiling Source Code' }),
+    ContractDecompileError: LogicError.extend({ code: 50702, name: 'Contract Verify Error, failed to decompile the contract' }),
+    ExtractMetadataError: LogicError.extend({ code: 50703, name: 'Contract Verify Error, failed to get metadata in Bytecode' }),
+    ContractNameError: LogicError.extend({ code: 50704, name: 'Contract Verify Error, the contract could not be found or the contract name was entered incorrectly' }),
 }
