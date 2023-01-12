143eff687ae87cb84298d03b1aa753561b21953e
Kang <kangjianhu@conflux-chain.org>
2023-01-12 17:15:04 +0800

    scan api log library

--- common/AppBase.js
+++ common/AppBase.js
@@ -7,7 +7,6 @@ const lodash = require('lodash');
 const Koaflow = require('koaflow');
 const requestLogger = require('koaflow/lib/middleware/requestLogger');
 const requestId = require('koaflow/lib/middleware/requestId');
-const Logger = require('@conflux-lib/logger');
 
 const { Conflux } = require('js-conflux-sdk');
 const WSServer = require('./lib/WSServer');
@@ -23,18 +22,7 @@ const TraceLog = require('./TraceLog');
 const ConfluxSDK = require('./ConfluxSDK');
 const {createLogger} = require("./utils");
 
-// eslint-disable-next-line no-unused-vars
-class ScanLog extends Logger {
-  log(level, object) {
-    if (!(object instanceof Object)) {
-      object = { message: `${object}` };
-    }
-    // console.log(`what's it ?`, object);
-    const time = new Date().toISOString();
-    const string = JSON.stringify({ time, level, ...object, ...this.tags });
-    return Promise.all(this.streams.map((stream) => stream.log(level, string)));
-  }
-}
+
 class AppBase extends Koaflow {
   constructor(config) {
     super();
