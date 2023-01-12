cdc6aac739d1f3987a7e95483e15b0e217a78563
Kang <kangjianhu@conflux-chain.org>
2023-01-12 18:29:29 +0800

    json format

--- scan-compiler/config/index.js
+++ scan-compiler/config/index.js
@@ -9,19 +9,11 @@ module.exports = {
 
   requestLogger: {
     level: 'info',
-    format: 'object',
+    format: 'json',
     request: { method: true, url: true, query: true, header: true },
     response: { status: true, message: true, duration: true },
   },
 
-  // level: [trace, debug, info, warn, error, fatal]
-  logger: {
-    tags: { name: 'compiler', nonce: Date.now() },
-    streams: [
-      { type: 'daily', path: '/log/info.log', level: 'info', days: 10 },
-      { type: 'daily', path: '/log/error.log', level: 'warn', days: 30 },
-    ],
-  },
 
   dingTalk: {
     accessToken: '',
