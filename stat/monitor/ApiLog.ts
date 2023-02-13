526ed5199826dcf98c82eec75312d9be4a2a3708
Kang <kangjianhu@conflux-chain.org>
2023-02-13 11:11:43 +0800

    auto add column on ApiLog

--- stat/monitor/ApiLog.ts
+++ stat/monitor/ApiLog.ts
@@ -1,14 +1,8 @@
-import {
-    Transaction,
-    Model,
-    DataTypes,
-    Sequelize,
-    Op,
-    UniqueConstraintError,
-    ModelStatic,
-    DatabaseError
-} from "sequelize";
+import {DataTypes, Model, Sequelize} from "sequelize";
 import {API_LOG_RT_LIMIT, KV} from "../model/KV";
+import QueryTypes = require("sequelize/types/lib/query-types");
+
+const requestIp = require('request-ip');
 
 export interface IApiLog {
     id?:number; path:string; query:string; rt:number; createdAt:Date;
@@ -21,6 +15,7 @@ export class ApiLog extends Model<IApiLog> implements IApiLog {
             path: {type: DataTypes.STRING(256), allowNull: false},
             query: {type: DataTypes.STRING(2048), allowNull: false},
             rt: {type: DataTypes.INTEGER, allowNull: false},
+            ip: {type: DataTypes.STRING(64), allowNull: false, defaultValue:''},
             createdAt: {type: DataTypes.DATE, allowNull: false},
         },{
             sequelize: seq, tableName: 'api_log', updatedAt: false,
@@ -31,6 +26,11 @@ export class ApiLog extends Model<IApiLog> implements IApiLog {
             ]
         })
     }
+}
+
+export async function checkApiLogIpField() {
+    const descRes = await ApiLog.sequelize.query(`desc ${ApiLog.getTableName()}`, {type: QueryTypes.SELECT})
+    console.log(`api log table`, descRes)
 }
 
 let rtThreshold = 1000
@@ -64,6 +64,7 @@ export async function saveApiLog(ctx:any, rt:number) {
     if (query) {
         query = decodeURIComponent(query);
     }
+    const ip = requestIp.getClientIp(ctx.request);
     ApiLog.create({
         path, query, createdAt: new Date(), rt,
     }).then()
