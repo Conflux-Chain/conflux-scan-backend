0c1b5f56ba9628624ec5cffd54d1560cc0ea26fb
zhenting.wang <argeric@qq.com>
2023-05-09 16:16:25 +0800

    UPDATE: swagger desc for active account stat

--- open-api/router/ApiRouter.ts
+++ open-api/router/ApiRouter.ts
@@ -197,8 +197,8 @@ function registerRouter(router: Router) {
     router.get('/statistics/contract', listContractStat);
     router.get('/statistics/account/cfx/holder', listCfxHolderStat);
     router.get('/statistics/account/growth', listAccountGrowthStat);
-    router.get('/statistics/account/active', listAccountActiveStat);
-    router.get('/statistics/account/tx/sender', listTransactionSenderStat);
+    router.get('/statistics/account/active', listTransactionSenderStat);
+    router.get('/statistics/account/active/overall', listAccountActiveStat);
     router.get('/statistics/transaction', listTransactionStat);
     router.get('/statistics/cfx/transfer', listCfxTransferStat);
     router.get('/statistics/token/transfer', listTokenTransferStat);
