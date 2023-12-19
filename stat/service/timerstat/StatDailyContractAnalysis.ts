f49db1e04118a6c39ba80b90c8f56cd89b70b26b
Argeric <argeric@qq.com>
2023-12-19 17:38:18 +0800

    UPDATE: optimize daily contract analysis (#575)
    * UPDATE: optimize daily contract analysis
    
    * UPDATE: use getTableName

--- stat/service/timerstat/StatDailyContractAnalysis.ts
+++ stat/service/timerstat/StatDailyContractAnalysis.ts
@@ -1,4 +1,4 @@
-import {Op} from 'sequelize'
+import {Op, QueryTypes} from 'sequelize'
 import {DailyContractStat} from "../../model/DailyContractStat";
 import {FullTransaction} from "../../model/FullBlock";
 import {CfxTransfer} from "../../model/CfxTransfer";
@@ -28,7 +28,6 @@ export class StatDailyContractAnalysis extends TimerStat{
             order:[["statTime","desc"]],
             limit: 1
         });
-
         return this.getStatRangeDay(lastStat, 1);
     }
 
@@ -37,20 +36,36 @@ export class StatDailyContractAnalysis extends TimerStat{
             attributes:['epoch'],
             where: {timestamp: {[Op.gte]: rangeEnd}},
             order:[['timestamp', 'asc']],
-            limit: 1
+            limit: 1,
         }).then(item => item?.epoch);
     }
 
     public async stat(rangeBegin: Date, rangeEnd: Date){
+        const [minEpoch, maxEpoch] = await Promise.all([
+            Epoch.findOne({where: {timestamp: {[Op.lt]: rangeBegin}}, order: [['timestamp', 'DESC']]}).then(epoch => {return epoch.epoch + 1}),
+            Epoch.findOne({where: {timestamp: {[Op.lt]: rangeEnd}}, order: [['timestamp', 'DESC']]}).then(epoch => {return epoch.epoch}),
+        ])
+
+        const [txMap, cfxTransferMap, erc20TransferMap, erc721TransferMap, erc1155TransferMap] = await Promise.all(
+            [
+                this.statByEpochRange(minEpoch, maxEpoch, FullTransaction),
+                this.statByEpochRange(minEpoch, maxEpoch, CfxTransfer),
+                this.statByEpochRange(minEpoch, maxEpoch, Erc20Transfer),
+                this.statByEpochRange(minEpoch, maxEpoch, Erc721Transfer),
+                this.statByEpochRange(minEpoch, maxEpoch, Erc1155Transfer),
+            ]
+        )
+
         const contractArray = await TraceCreateContract.findAll({attributes: ['to', 'blockTime'], raw: true}) || [];
         await this.addInternalContract(contractArray);
-
+        const statInfo = {txMap, cfxTransferMap, erc20TransferMap, erc721TransferMap, erc1155TransferMap}
         for (const [index, contract] of contractArray.entries()) {
             const contractStatDb = await DailyContractStat.findOne({where: {hex40id: contract.to, statTime: rangeBegin}});
             if(contractStatDb){
                 continue;
             }
-            const stat = await this.statDailyByAddress(contract, rangeBegin, rangeEnd);
+
+            const stat = await this.statDailyByAddress(contract, rangeBegin, rangeEnd, statInfo);
             if(!stat){
                 continue;
             }
@@ -63,57 +78,45 @@ export class StatDailyContractAnalysis extends TimerStat{
     }
 
     // ------------------------------- biz -----------------------------------
-    private async statDailyByAddress(contract, rangeBegin: Date, rangeEnd: Date){
+    private async statDailyByAddress(contract, rangeBegin: Date, rangeEnd: Date, statInfo){
         const addressId = contract.to;
         const blockTime = contract.blockTime;
         if(blockTime >= rangeEnd.getTime() / 1000){
             return undefined;
         }
 
-        const txCount = await FullTransaction.count({
-            where: {
-                [Op.and]:
-                    [
-                        {createdAt: {[Op.gte]:rangeBegin}},
-                        {createdAt: {[Op.lt]:rangeEnd}},
-                        {[Op.or]: [{fromId: addressId}, {toId: addressId}]},
-                    ]
-            }});
-        const cfxTransferCount = await CfxTransfer.count({
-            where: {
-                [Op.and]:
-                    [
-                        {createdAt: {[Op.gte]:rangeBegin}},
-                        {createdAt: {[Op.lt]:rangeEnd}},
-                        {[Op.or]: [{fromId: addressId}, {toId: addressId}]},
-                    ]
-            }});
-        const tokenTransfer = await this.getTokenTransferStat(addressId, rangeBegin, rangeEnd);
-
-        return {hex40id: addressId, statTime: rangeBegin, tx: txCount, cfxTransfer: cfxTransferCount,
-            tokenType: tokenTransfer.type, tokenTransfer: tokenTransfer.count};
+        return {
+            hex40id: addressId,
+            statTime: rangeBegin,
+            tx: statInfo.txMap[addressId] || 0,
+            cfxTransfer: statInfo.cfxTransferMap[addressId] || 0,
+            tokenTransfer: statInfo.erc20TransferMap[addressId] || statInfo.erc20TransferMap[addressId] || statInfo.erc20TransferMap[addressId] || 0
+        };
     }
 
-    private async getTokenTransferStat(addressId, rangeBegin, rangeEnd) {
-        const typeInfo = await TokenQuery.detectTokenType({hex40id: addressId});
-        const type = typeInfo?.type;
+    private async statByEpochRange(minEpoch, maxEpoch, model) {
+        const t1 = TraceCreateContract.getTableName()
+        const t2 = model.getTableName()
+        const sqlWithFrom = `select tcc.to as addr, count(*) as cntr from ${t1} tcc left join ${t2} tmp on tcc.to = tmp.toId
+                        where epoch >= ? and epoch < ? group by tcc.to`
+        const sqlWithFromTo = `select t.addr as addr, sum(t.cntr) as cntr from (
+                        (select count(*) as cntr, tcc.to as addr from ${t1} tcc left join ${t2} tmp on tcc.to = tmp.toId
+                        where epoch >= ? and epoch < ? group by tcc.to) 
+                        union all 
+                        (select count(*) as cntr, tcc.to as addr from ${t1} tcc left join ${t2} tmp on tcc.to = tmp.fromId
+                        where epoch >= ? and epoch < ? group by tcc.to)
+                        ) t group by t.addr`
 
-        let model;
-        if(type === CONST.TRANSFER_TYPE.ERC20) {
-            model = Erc20Transfer;
-        } else if(type === CONST.TRANSFER_TYPE.ERC721) {
-            model = Erc721Transfer;
-        } else if(type === CONST.TRANSFER_TYPE.ERC1155) {
-            model = Erc1155Transfer;
-        }
+        const isStatTx = model === FullTransaction
+        const statArray = await TraceCreateContract.sequelize.query(isStatTx ? sqlWithFrom : sqlWithFromTo, {
+            type: QueryTypes.SELECT,
+            replacements: isStatTx ? [minEpoch, maxEpoch] : [minEpoch, maxEpoch, minEpoch, maxEpoch],
+            raw: true,
+        })
 
-        const count = await model?.count({
-            where: {
-                [Op.and]: [{contractId: typeInfo?.hex40id},
-                    {createdAt: {[Op.gte]: rangeBegin}}, {createdAt: {[Op.lt]: rangeEnd}}]
-            }
-        });
-        return {type, count};
+        const result = {}
+        statArray.forEach(stat => result[stat['addr']] = stat['cntr'])
+        return result;
     }
 
     private async addInternalContract(contractList){
