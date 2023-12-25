ae3055d630f8cd5022c9d3dada06f8620c94ca44
Kang <kangjianhu@conflux-chain.org>
2023-12-25 15:36:28 +0800

    add machine in biz field

--- scan-api/index.js
+++ scan-api/index.js
@@ -11,13 +11,13 @@ const compilerRpc = proxy[Object.keys(proxy)[0]];
 setInterval(async ()=>{
   try {
     await superagent.get(compilerRpc)
-    await HeartBeatBean.upsert({key: KEY_COMPILER, updatedAt: new Date()})
+    await HeartBeatBean.upsert({key: `${KEY_COMPILER}_${config.machine}`, updatedAt: new Date()})
   } catch (e) {
     console.log(`failed to call compiler rpc ${compilerRpc}`, e)
   }
 }, 10_000)
 // report scan api heart beat
-repeatHeartBeat(KEY_SCAN_API)
+repeatHeartBeat(`${KEY_SCAN_API}_${config.machine}`)
 
 const app = new App(config);
 
