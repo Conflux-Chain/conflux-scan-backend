c19f1a48f3ab4db0916db36f7f957ad2a5bf07a3
Argeric <argeric@qq.com>
2023-12-27 11:23:30 +0800

    UPDATE: api total supply (#583)

--- scan-api/router/supply.js
+++ scan-api/router/supply.js
@@ -1,6 +1,7 @@
 const Koaflow = require('koaflow');
 const { Drip } = require('js-conflux-sdk');
 const jsonrpc = require('./jsonrpc');
+const { formatDecimal } = require('../../stat/dist/service/common/utils');
 
 const router = new Koaflow.Router();
 
@@ -9,8 +10,17 @@ router.get('/circulating',
 
   // eslint-disable-next-line prefer-arrow-callback
   async function ({ totalCirculating }) {
-    return Drip(totalCirculating).toCFX();
+    return formatDecimal(Drip(totalCirculating).toCFX(), 2);
   },
+);
+
+router.get('/total',
+    jsonrpc.methodFlow('supply'),
+
+    // eslint-disable-next-line prefer-arrow-callback
+    async function ({ totalIssued, nullAddressBalance }) {
+        return formatDecimal(Drip(`${BigInt(totalIssued) - BigInt(nullAddressBalance)}`).toCFX(), 2);
+    },
 );
 
 module.exports = router;
