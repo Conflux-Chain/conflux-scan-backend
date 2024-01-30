01ec5af53a56c797bb695d5978a0ba9e6498a5a6
Kang <kangjianhu@conflux-chain.org>
2024-01-30 16:35:29 +0800

    syntax

--- scan-api/router/MyJsonRpcFlow.js
+++ scan-api/router/MyJsonRpcFlow.js
@@ -10,8 +10,9 @@ class MyJsonRpcFlow extends JsonRPCFlow {
   // super function doesn't return a value, just wrap that.
   method_(method, ...flowArray) {
     super.method(method, ...flowArray)
+    const thatMethod = this.methods[method];
     return async function(arg, next, end) {
-      this.methods[method].call(this, [arg], next, end);
+      thatMethod.call(this, [arg], next, end);
     }
   }
   methodFlow(method) {
