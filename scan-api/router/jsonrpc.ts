ca296c340b6d34bf5a2af2de90530c8829d380bf
Kang <kangjianhu@conflux-chain.org>
2024-03-12 11:04:47 +0800

    add rpc config in /frontend response 2

--- scan-api/router/jsonrpc.ts
+++ scan-api/router/jsonrpc.ts
@@ -1,6 +1,6 @@
 import {ScanCtx} from "../service/index";
 import {Op} from "sequelize";
-import {KEY_CONFURA_URL, KEY_CORE_OPEN_API_URL, KEY_OPEN_API_URL} from "../../stat/model/KV";
+import {KEY_CONFURA_URL, KEY_CORE_API_URL, KEY_CORE_OPEN_API_URL, KEY_OPEN_API_URL} from "../../stat/model/KV";
 
 const lodash = require('lodash');
 const Big = require('big.js');
@@ -150,7 +150,9 @@ jsonrpc.method('frontend',
         return { key: contract.key, name: contract.name, address: contract.address[networkId] };
       });
       frontedConfig = { networkId, networks, contracts };
-      const urls = await KV.findAll({where: {"key": {[Op.in]: [KEY_OPEN_API_URL, KEY_CORE_OPEN_API_URL, KEY_CONFURA_URL]}}})
+      const urls = await KV.findAll({where: {"key": {[Op.in]:
+                      [KEY_OPEN_API_URL, KEY_CORE_OPEN_API_URL, KEY_CONFURA_URL, KEY_CORE_API_URL]
+      }}})
       urls.forEach(config=>{
           frontedConfig[config.key] = config.value;
       })
