1be89c906f02b9b96240fc58e80005b86f3216c0
Kang <kangjianhu@conflux-chain.org>
2024-05-09 15:12:46 +0800

    evm response

--- stat/service/tool/AbiInfoTool.ts
+++ stat/service/tool/AbiInfoTool.ts
@@ -1,5 +1,6 @@
 import {init} from "./FixDailyTokenStat";
 import {AbiInfo} from "../../model/ContractInfo";
+import {sleep} from "./ProcessTool";
 const superagent = require('superagent')
 async function run() {
     const [,,host_] = process.argv
@@ -7,8 +8,14 @@ async function run() {
     let skip = 0
     do {
         const res = await superagent.get(`${host}/stat/devops/view-table?t=abi_info&skipStr=${skip}`)
-        const body = res.body?.data
+        // core and evm have different response
+        const body = res.body?.data || res.body?.result
+        if (body.code === 429) {
+            await sleep(1000)
+            continue
+        }
         if (!body?.list?.length) {
+            console.log(`error ? `, res.body || res)
             break;
         }
         // console.log(`abi info:`, body)
