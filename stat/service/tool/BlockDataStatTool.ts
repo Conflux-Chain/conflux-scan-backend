f80870129afc63fa58e431b8e8c66a7ea0b8ce34
Kang <kangjianhu@conflux-chain.org>
2024-05-15 12:03:10 +0800

    fix block reward

--- stat/service/tool/BlockDataStatTool.ts
+++ stat/service/tool/BlockDataStatTool.ts
@@ -5,7 +5,7 @@ import {MinerBlock} from "../../model/MinerBlock";
 import {Epoch} from "../../model/Epoch";
 import {initCfxSdk} from "../common/utils";
 import {FullBlockService} from "../FullBlockService";
-import {loadMaxBlockEpoch} from "../../model/FullBlock";
+import {sleep} from "./ProcessTool";
 async function run(startDay, endDay) {
     const config = await init();
     if (args.includes('miner')) {
@@ -16,6 +16,8 @@ async function run(startDay, endDay) {
     } else if (args.includes('fixRecentMinerStat')) {
         await fixRecentMinerStat()
         return;
+    } else if (args.includes('fixReward')) {
+        return fixBlockTxFeeNewer(config)
     } else if (args.includes('fixTxFee')) {
         await fixBlockTxFee(config)
         return
@@ -29,6 +31,27 @@ async function fixRecentMinerStat() {
         now.setHours(now.getHours() - 1)
         console.log(` fix at ${now.toISOString()}`)
     }
+}
+async function fixBlockTxFeeNewer(config: StatConfig) {
+    const cfx = await initCfxSdk(config.conflux);
+    let diff = parseInt(args[1])
+    let epochConfirmed = await cfx.getEpochNumber('latest_confirmed')
+    let epoch = epochConfirmed - diff
+    console.log(`-----  networkId ${cfx.networkId} ------ start at ${epoch} , diff ${diff}`)
+    const svc = new FullBlockService(cfx)
+    async function repeat() {
+        if (epoch >= epochConfirmed) {
+            await sleep(5_000)
+            epoch = await cfx.getEpochNumber('latest_confirmed').catch(()=>epoch)
+        }
+        await svc.fillBlockReward(epoch).catch(console.log);
+        if (epoch % 10 === 0) {
+            console.log(` fix epoch ${epoch} , confirmed ${epochConfirmed}`)
+        }
+        epoch ++;
+        setTimeout(repeat, 0)
+    }
+    repeat().then()
 }
 async function fixBlockTxFee(config: StatConfig) {
     const cfx = await initCfxSdk(config.conflux);
