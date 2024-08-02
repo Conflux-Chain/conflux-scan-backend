ef404c8d17cc11f9e8d104de5fb5dd430a9a9a94
Kang <kangjianhu@conflux-chain.org>
2024-08-02 18:19:03 +0800

    cache rpc response

--- stat/service/common/ScanHttpProvider.ts
+++ stat/service/common/ScanHttpProvider.ts
@@ -1,3 +1,8 @@
+import * as fs from "fs";
+import {URL} from "url";
+import {post} from "./http";
+import {readFileSync} from "fs";
+
 const HttpProvider = require("js-conflux-sdk/src/provider/HttpProvider")
 const superagent = require('superagent');
 const Agent = require('agentkeepalive');
@@ -7,19 +12,49 @@ export class ScanHttpProvider extends HttpProvider {
     tag: string
     times = 0
     agent = new Agent({maxSockets: 100, timeout:73000})
+    conf: {readCache: boolean, writeCache: boolean, cachePath: string}
     methodTimes = {}
     constructor(conf, tag) {
         super(conf);
         // this.headers = {Connection: "keep-alive"}
         this.tag = tag
+        this.url = new URL(conf.url)
+        this.url.headers = {
+            'Connection': 'keep-alive',
+            'Content-type': 'application/json'
+        }
+        this.conf = conf;
+        if ((conf.writeCache || conf.readCache) ) {
+            if (!conf.cachePath) {
+                console.log(`${__filename} must set cache path`)
+                process.exit(9)
+            }
+            // test write
+            try {
+                fs.writeFileSync(`${conf.cachePath}/testWrite.txt`, "a test file")
+            } catch (e) {
+                console.log(`failed to write in cache path`, e)
+                process.exit(9)
+            }
+        }
     }
 
     async _doRequest(data) {
-        return limit(()=>this.request0(data))
+        return limit(()=>this.conf.writeCache ? this.requestAndCache(data) : this.request0(data))
+    }
+    async requestAndCache(data) {
+        const text = await post(this.url, data)
+        return cacheRes(text, data, this.conf.cachePath);
     }
     async request0(data) {
+        if (this.conf.readCache) {
+            const cache = readCache(data.method, data.params, this.conf.cachePath)
+            if (cache) {
+                // console.log(`hit cache`)
+                return cache;
+            }
+        }
         // await new Promise(r=>setTimeout(r, 2000))
-        this.times ++
         // this.methodTimes[data.method] = (this.methodTimes[data.method] || 0) + 1
         // console.log(` ----- ${this.tag}, total times ${this.times}: request rpc ${data.method
         // } x ${this.methodTimes[data.method]}, header `, this.headers)
@@ -35,4 +70,74 @@ export class ScanHttpProvider extends HttpProvider {
         }
         return body || {};
     }
+}
+
+function cacheRes(text : string, data: any, cacheDir: string) {
+    let body: any;
+    try {
+        body = JSON.parse(text);
+    } catch (e) {
+        console.log(`bad json text`, text)
+        throw e;
+    }
+    // console.log(`${__filename} method is `, data.method)
+    const parseParamFn = CacheConfig[data.method];
+    if (parseParamFn && body.result) {
+        const path = `${cacheDir}/${data.method}_${parseParamFn(data.params)}.json.tmp`;
+        const writePromise= fs.promises.writeFile(path, text)
+        body.result[TempCachePathProp] = path;
+        body.result[WriteCachePromise] = writePromise;
+    }
+    return body
+}
+
+export const CacheConfig = {
+    cfx_getBlocksByEpoch: ([no])=>{
+        return BigInt(no); // NO is a hex str
+    },
+    cfx_getBlockByHash: ([hash, detail])=>{
+        return `${hash}_${detail}`
+    },
+    cfx_getEpochReceipts: ([no])=>{
+        return BigInt(no);
+    }
+}
+
+export const TempCachePathProp = "tempCachePath"
+export const WriteCachePromise = "writeCachePromise"
+
+export async function onlineCache(obj: any) {
+    const path = obj[TempCachePathProp];
+    if (!path) {
+        // console.log(`${__filename} path is empty`, path)
+        return
+    }
+    try {
+        await obj[WriteCachePromise];
+    } catch (e) {
+        console.log(`write cache promise failed`, e)
+        return
+    }
+    fs.renameSync(path, path.slice(0, path.length - 4));
+    // console.log(`online cache ${path}`)
+}
+
+function readCache(method, params, cacheDir: string) : any {
+    const parseParamFn = CacheConfig[method];
+    if (parseParamFn) {
+        const path = `${cacheDir}/${method}_${parseParamFn(params)}.json`;
+        let text: string;
+        try {
+            text = readFileSync(path, "utf-8").toString();
+        } catch (e) {
+            console.log(`failed to load cache file`, e.message)
+            return undefined
+        }
+        try {
+            return JSON.parse(text)
+        } catch (e) {
+            console.log(`failed to parse cache at ${path} \n content [ ${text} ]\n`, e)
+        }
+    }
+    return undefined
 }
---
+++ stat/service/common/http.ts
@@ -0,0 +1,39 @@
+import * as http from "http";
+import {RequestOptions} from "http";
+
+// superagent doesn't expose an interface to intercept response,
+// use this one to access the original text.
+export async function post({host, port, protocol, headers}: RequestOptions, data: any) : Promise<string> {
+	const postData = JSON.stringify(data);
+	const forkOpt = {host, port, protocol, headers: {...headers}}
+	forkOpt.headers['Content-Length'] = Buffer.byteLength(postData);
+
+	return new Promise((resolve, reject) => {
+
+		const req = http.request(forkOpt, (res) => {
+			// console.log(`STATUS: ${res.statusCode}`);
+			// console.log(`HEADERS: ${JSON.stringify(res.headers)}`);
+			if (res.statusCode != 200) {
+				reject(`chain rpc returns ${res.statusCode}`)
+				return
+			}
+			let buff = "";
+			res.setEncoding('utf8');
+			res.on('data', (chunk) => {
+				// console.log(`BODY: ${chunk}`);
+				buff += chunk;
+			});
+			res.on('end', () => {
+				resolve(buff)
+			});
+		});
+
+		req.on('error', (e) => {
+			reject(e)
+		});
+
+		// Write data to request body
+		req.write(postData);
+		req.end();
+	})
+}
