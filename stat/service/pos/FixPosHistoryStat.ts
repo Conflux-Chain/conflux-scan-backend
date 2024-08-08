51b16d5eabd33ac2fcbfe92b01fffea55aa0424a
Kang <kangjianhu@conflux-chain.org>
2024-08-08 17:29:48 +0800

    fix pos apy 3

--- stat/service/pos/FixPosHistoryStat.ts
+++ stat/service/pos/FixPosHistoryStat.ts
@@ -102,6 +102,7 @@ async function main() {
 }
 
 // node stat/service/pos/FixPosHistoryStat.js fixDailyStaking
+// node stat/service/pos/FixPosHistoryStat.js fixDailyApy
 if (module === require.main) {
     main().then()
 }
