21a50ffc61223ac8fc58129acc59db752c4a69f9
Kang <kangjianhu@conflux-chain.org>
2024-08-08 17:49:56 +0800

    /supply

--- scan-api/router/supply.ts
+++ scan-api/router/supply.ts
@@ -1,3 +1,5 @@
+import {ScanCtx} from "../service/index";
+
 const {Router} = require('../../koaflow/src/router');
 const { Drip } = require('js-conflux-sdk');
 const {jsonrpc} = require('./jsonrpc');
@@ -6,19 +8,24 @@ const { formatDecimal } = require('../../stat/service/common/utils');
 const router = new Router();
 
 router.get('/circulating',
-  jsonrpc.methodFlow('supply'),
 
   // eslint-disable-next-line prefer-arrow-callback
-  async function ({ totalCirculating }) {
+  async function () {
+      const { app: { service }, } = this as ScanCtx;
+      const data = service.homeDashboard.getData();
+      // @ts-ignore
+    const totalCirculating = data?.supplyInfo?.totalCirculating ?? 0
     return formatDecimal(Drip(totalCirculating).toCFX(), 2);
   },
 );
 
 router.get('/total',
-    jsonrpc.methodFlow('supply'),
-
     // eslint-disable-next-line prefer-arrow-callback
-    async function ({ totalIssued, nullAddressBalance }) {
+    async function () {
+      const { app: { service }, } = this as ScanCtx;
+      const data = service.homeDashboard.getData()?.supplyInfo || {totalIssued: 0, nullAddressBalance: 0};
+      // @ts-ignore
+      const { totalIssued, nullAddressBalance } = data;
         return formatDecimal(Drip(`${BigInt(totalIssued) - BigInt(nullAddressBalance)}`).toCFX(), 2);
     },
 );
