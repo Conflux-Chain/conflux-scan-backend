a2a1351fbe6bc4efb564015531feb9e55538a785
Kang <kangjianhu@conflux-chain.org>
2024-09-10 16:45:38 +0800

    use addrId

--- stat/service/TxnQuery.ts
+++ stat/service/TxnQuery.ts
@@ -87,7 +87,7 @@ export class TxnQuery{
         const sumGas = list.map(row=>BigInt(row['gas'])).reduce((a,b)=>a+b);
         const hexMap = await idHex40Map(list.map(row=>row['addrId']));
         list.forEach(row=>{
-            row['hex'] = ethers.utils.getAddress(`0x${hexMap.get(row['fromId'])}`);
+            row['hex'] = ethers.utils.getAddress(`0x${hexMap.get(row['addrId'])}`);
             !NoCoreSpace && (row['base32'] = TxnQuery.base32(row['hex'], StatApp.networkId));
         })
         let result = {totalGas: sumGas, list, minTime};
