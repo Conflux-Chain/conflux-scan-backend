b3b2662d8cdd85f0db907c55d25373710c916554
Kang <kangjianhu@conflux-chain.org>
2024-09-24 10:27:13 +0800

    supply.ts

--- scan-api/router/supply.ts
+++ scan-api/router/supply.ts
@@ -1,17 +1,16 @@
 import {ScanCtx} from "../service/index";
 
 import * as KoaRouter from "koa-router";
-const {router_get} = require("../../koaflow/src/koaHelper");
 const {Drip} = require('js-conflux-sdk');
 const {formatDecimal} = require('../../stat/service/common/utils');
 
 const router = new KoaRouter();
 
-router_get(router, '/circulating',
+router.get('/circulating',
 
 	// eslint-disable-next-line prefer-arrow-callback
-	async function () {
-		const {app: {service},} = this as ScanCtx;
+	async function (ctx) {
+		const {app: {service},} = ctx as unknown as ScanCtx;
 		const data = service.homeDashboard.getData();
 		// @ts-ignore
 		const totalCirculating = data?.supplyInfo?.totalCirculating ?? 0
@@ -22,10 +21,10 @@ router_get(router, '/circulating',
 	},
 );
 
-router_get(router, '/total',
+router.get('/total',
 	// eslint-disable-next-line prefer-arrow-callback
-	async function () {
-		const {app: {service},} = this as ScanCtx;
+	async function (ctx) {
+		const {app: {service},} = ctx as unknown as ScanCtx;
 		const data = service.homeDashboard.getData()?.supplyInfo || {totalIssued: 0, nullAddressBalance: 0};
 		// @ts-ignore
 		const {totalIssued, nullAddressBalance} = data;
