2b785528c2e267d97e673a464bfae9942deaa58f
Tigo9527 <68840946+Tigo9527@users.noreply.github.com>
2024-10-21 10:54:46 +0800

    Influx conf and chain height pushing (#720)
    * port, protocol
    
    * report chain height to influx
    
    ---------
    
    Co-authored-by: Kang <kangjianhu@conflux-chain.org>

--- stat/monitor/InfluxWorker.ts
+++ stat/monitor/InfluxWorker.ts
@@ -10,6 +10,11 @@ import {FullBlock} from "../model/FullBlock";
 import {HeartBeatBean} from "../model/HeartBeat";
 import {KEY_1155data_EPOCH, KV} from "../model/KV";
 import {Op} from "sequelize";
+import {Conflux} from "js-conflux-sdk";
+import {initCfxSdk} from "../service/common/utils";
+
+let cfx: Conflux;
+
 async function copy(inf: InfluxDB, model:any, biz, epochField: Function = (a)=>a.epoch) {
     // model = TaskCfxTransfer;
     const max = await model.findOne({order: [['epoch', 'desc']]})
@@ -67,7 +72,12 @@ async function copyAll(inf: InfluxDB) {
     await copy(inf, Epoch, 'sync-epoch')
     await copy(inf, FullBlock, 'sync-block-and-tx')
     // influx worker itself
-    await write(inf, measurement, {epoch: Date.now(), createdAt: new Date(), biz: 'influx-worker'})
+    await write(inf, measurement, {epoch: Date.now(), createdAt: new Date(), biz: 'influx-worker'});
+    await cfx.getEpochNumber().then(res=>{
+        return write(inf, measurement, {epoch: res, createdAt: new Date(), biz: 'chain-height'});
+    }).catch(e=>{
+        console.log(`failed to get epoch number`, e)
+    })
     await heartBeat(inf)
     // await epochCursorInConfig(inf)
     console.log(`---`)
@@ -76,9 +86,9 @@ class EpochMax {
     epoch: number; biz:string; createdAt: Date;
 }
 let measurement = 'sync_epoch_3';
-function connectInflux({host, database, username, password}) {
+function connectInflux({host, database, username, password,  port, protocol}) {
     const influx = new InfluxDB({
-        host,        database, username, password,
+        host,        database, username, password, port, protocol,
         schema: [
             {
                 measurement,
@@ -126,12 +136,13 @@ async function setup() {
     // host = 'http://influxdb-luhhh4.conflux-chain.org.cn'
     const config = await init();
     console.log(`------init done-----`)
-    const {host, database, username, password, measurement: confMeasurement} = config.influxDB
+    cfx = await initCfxSdk(config.conflux);
+    const {host, database, username, password,  port, protocol, measurement: confMeasurement} = config.influxDB
     if (confMeasurement) {
         measurement = confMeasurement
     }
     console.log(`influx db is ${host} ${database} user ${username} measurement ${measurement}`)
-    const inf = connectInflux({host, database, username, password});
+    const inf = connectInflux({host, database, username, password,  port, protocol});
     // await test(inf);
     await copyAll(inf)
     setInterval(()=>{
