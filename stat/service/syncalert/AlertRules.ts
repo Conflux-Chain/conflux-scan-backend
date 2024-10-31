ef9d6fb6ba431c7e2bbd4a959be3b325ea8059e0
Kang <kangjianhu@conflux-chain.org>
2024-10-31 17:54:35 +0800

    debug

--- stat/service/syncalert/AlertRules.ts
+++ stat/service/syncalert/AlertRules.ts
@@ -12,6 +12,7 @@ interface IMeterData {
 const map:Map<string, IMeterData> = new Map<string, IMeterData>();
 
 export function pushMeter(metrics: IMetric[]) {
+	let cnt = 0;
 	for(const m of metrics) {
 		const {tags:{syncType}, fields: {latestSynced, syncGap}} = m;
 		const meterData = map.get(m.tags.syncType);
@@ -25,8 +26,10 @@ export function pushMeter(metrics: IMetric[]) {
 			continue
 		}
 		meterData.counter ++;
+		cnt = meterData.counter;
 		meterData.meterGap.mark(syncGap);
 		meterData.meterGrowth.mark(Math.max(latestSynced - meterData.lastV, 0));
+		meterData.lastV = latestSynced;
 		//
 		[meterData.meterGap, meterData.meterGrowth].forEach(m=>{
 			console.log(`meter info : ${m.getName().padEnd(20, ' ')} 1m ${m.get1MinuteRate() * 60} 5m ${m.get5MinuteRate() * 60
@@ -41,13 +44,13 @@ export function pushMeter(metrics: IMetric[]) {
 		{//var scope block
 			const v5mGrowth = meterData.meterGrowth.get5MinuteRate() * 60 * 5; // the returned value is based on 1 second.
 			if (v5mGrowth < threshold) {
-				console.log(`height doesn't grow, ${meterData.name}, 5minutes, v ${v5mGrowth} < ${threshold}`);
+				console.log(`height doesn't grow, ${meterData.name}, in last 5 minutes, total growth is ${v5mGrowth} < ${threshold}`);
 			}
 		}
 		const v5mGap_per_1m = meterData.meterGap.get5MinuteRate() * 60;
 		if (v5mGap_per_1m > threshold) {
-			console.log(`gap is too large, ${meterData.name}, 5minutes, v ${v5mGap_per_1m} > ${threshold}`);
+			console.log(`gap is too large, ${meterData.name}, in last 5 minutes, gap per minute is ${v5mGap_per_1m} > ${threshold}`);
 		}
 	}
-	console.log(`${__filename} ok`)
+	console.log(`${__filename} ok , round ${cnt}`)
 }
