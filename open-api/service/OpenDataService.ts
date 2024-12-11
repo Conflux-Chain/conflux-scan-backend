7bff35083c82d090bbd6522a76b6c12877c0e3b2
Tigo9527 <68840946+Tigo9527@users.noreply.github.com>
2024-12-11 18:18:46 +0800

    Data api : accounts (#753)
    * data api: accounts

---
+++ open-api/service/OpenDataService.ts
@@ -0,0 +1,28 @@
+import {intParam, mustBeEnumParamIfPresent, mustBeIntParamIfPresent} from "../../stat/service/common/utils";
+import {Hex40Map} from "../../stat/model/HexMap";
+import {Op} from "sequelize";
+import {fmtAddr, StatApp} from "../../stat/StatApp";
+import {setBody} from "../router/middleware";
+import {LIMIT_MAX} from "../../stat/router/ParamChecker";
+import {Errors} from "../../stat/service/common/LogicError";
+
+export async function listAccountsByCursor(ctx) {
+	mustBeIntParamIfPresent(ctx.request.query, "id", "limit");
+	mustBeEnumParamIfPresent(ctx.request.query, 'sort', ['DESC','ASC']);
+	let {id, sort = 'DESC'} = ctx.request.query;
+	const limit = intParam(ctx.request.query, "limit", 10);
+	if (limit > LIMIT_MAX) {
+		throw new Errors.ParameterError(`Parameter <limit exceeds ${LIMIT_MAX}`);
+	}
+	const idOption = {id:{[sort == "ASC" ? Op.gt : Op.lt] : id}};
+	if (id == undefined) {
+		delete idOption['id']
+	}
+	const list = await Hex40Map.findAll({where: idOption, order: [['id', sort]], limit, raw: true});
+
+	const addr= list.map(bean=>{
+		return {address: fmtAddr(`0x${bean.hex}`, StatApp.networkId), id: bean.id}
+	})
+
+	setBody(ctx, addr)
+}
