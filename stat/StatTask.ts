f1f1cad36a14b0c9f40f18e73232fa38ef914dcc
Kang <kangjianhu@conflux-chain.org>
2025-02-08 14:10:39 +0800

    config of app

--- stat/StatTask.ts
+++ stat/StatTask.ts
@@ -93,7 +93,7 @@ async function main() {
     await statDailyPowReward.schedule(1000 * 1);
     //
     if (config.syncTokenSecurityAudit) {
-        const tokenQuery = new TokenQuery({cfx})
+        const tokenQuery = new TokenQuery({cfx, config})
         const tokenAudit = new TokenSecurityAuditSync({tokenQuery})
         await tokenAudit.schedule()
         await tokenAudit.scheduleRecently()
