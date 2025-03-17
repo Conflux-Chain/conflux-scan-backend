97e727d0fbd15454ef49ead9414d53ce8f0517c4
Kang <kangjianhu@conflux-chain.org>
2025-03-17 16:21:26 +0800

    execution reverted; use token tool cache.

--- stat/service/tool/TokenTool.ts
+++ stat/service/tool/TokenTool.ts
@@ -760,6 +760,7 @@ function handlerCallError(biz: string, err: Error) {
     || err.message === '{"message":"length not match","expect":2,"got":0,"stream":{"string":"0x","index":2}}'
     || err.message === '{"message":"length not match","expect":64,"got":0,"stream":{"string":"0x","index":2}}'
     || err.message === 'Transaction execution failed' // internal contract outputs
+    || err.message?.startsWith('execution reverted') // evm
     ) {
         console.log(`failed to call ${biz} , message ${err.message}`)
         return;
