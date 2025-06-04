889f5e4caa17354e6d5b746ce376ba349c7fea5e
Kang <kangjianhu@conflux-chain.org>
2025-06-04 18:08:01 +0800

    decodeAnnouncement

--- tools/Tools.ts
+++ tools/Tools.ts
@@ -1,5 +1,7 @@
 import {ConfigInstance, loadConfig} from "../stat/config/StatConfig";
 import {dingMsg} from "../stat/monitor/Monitor";
+import {TokenTool} from "../stat/service/tool/TokenTool";
+import {initCfxSdk} from "../stat/service/common/utils";
 
 export default {}
 
@@ -10,6 +12,28 @@ async function main() {
 		await dingMsg(`test ding, possible keywords are: cfx, alert scan. arg1: [${arg1}]`, ConfigInstance.dingTalkToken)
 	} else if (cmd === 'ding-dev') {
 		await dingMsg(`test ding, possible keywords are: cfx, alert scan. arg1: [${arg1}]`, ConfigInstance.dingDevToken)
+	} else if (cmd === 'decodeAnnouncement') {
+		cfg.conflux.url = 'http://net8888cfx.confluxrpc.com';
+		let hash = '0xcdcc5a24474e627fca12f0a29d0f5cfcedac6d9337b8f933c11f559fad4a273c';
+		const cfx = await initCfxSdk(cfg.conflux);
+		const tool = new TokenTool(cfx);
+		const rcpt = await cfx.getTransactionReceipt(hash)
+		for (const log of rcpt.logs) {
+			let event = tool.decodeAnnouncePlus(log);
+			console.log(`event `, event);
+			if (!event) {
+				continue
+			}
+			const announce: any = event;
+			const key = Buffer.from(announce.key, 'base64').toString();
+			const decodedBase64 = Buffer.from(announce.value, 'base64').toString();
+			console.log(`decoded base64: ${decodedBase64}`);
+			console.log(`key: ${key}`);
+			console.log(`raw value`, announce.value.toString());
+		}
+		const str = '["function workMyDirefulOwner(uint256,uint256)", "event Transfer(address indexed from, address indexed to, address value)"]';
+		const b64 = Buffer.from(str).toString('base64');
+		console.log(`b64: ${b64}`);
 	} else {
 		console.log(`nothing [${cmd}]`)
 	}
@@ -20,3 +44,4 @@ if (module == require.main) {
 }
 
 // node tools/Tools.js ding
+// node tools/Tools.js decodeAnnouncement
