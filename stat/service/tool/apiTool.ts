1c05d173d4fbf6f518c2b343540c4a422eea83df
Kang <kangjianhu@conflux-chain.org>
2025-07-02 11:07:54 +0800

    patchAddressInfo 5

--- stat/service/tool/apiTool.ts
+++ stat/service/tool/apiTool.ts
@@ -3,7 +3,7 @@ import {getAccountQuery} from "../AccountQuery";
 export async function patchAddressInfo(list: any[], fromKey: string, toKey: string) {
 	let addressArray = [];
 	list.forEach((tx) => {
-		fromKey && addressArray.push(tx[fromKey].toString());
+		tx[fromKey] && addressArray.push(tx[fromKey].toString());
 		tx[toKey] && (addressArray.push(tx[toKey].toString()));
 	});
 	const accountQuery = getAccountQuery();
