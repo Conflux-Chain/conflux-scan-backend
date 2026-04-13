327ca5d38cbc1e363117c6fac927c371d3e0536f
Kang <kangjianhu@conflux-chain.org>
2026-04-13 11:33:26 +0800

    PR: name map.

--- stat/service/eip/eip7702.ts
+++ stat/service/eip/eip7702.ts
@@ -315,7 +315,13 @@ export async function listGlobalAuthAction(ctx) {
 	mustBeAddressParamIfPresent(ctx.request.query, StatApp.networkId, StatApp.isEVM, 'author', 'address', 'txSender');
 	const {author, address, txSender} = ctx.request.query;
 	const result = await listAuthAction({author, address, txSender, skip, limit});
-	await getAccountQuery().patchAddressInfo(result.list, 'txSender', 'address');
+
+	const addresses = new Set(result.list.flatMap((item: any) => [item.txSender, item.address]).filter(Boolean));
+	result['nameMap'] = await getAccountQuery().list(
+		[...addresses],
+		{ withContractInfo: true, withNameTagInfo: true, withENSInfo: true }
+	);
+
 	setBody(ctx, result);
 }
 
