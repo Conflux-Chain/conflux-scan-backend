9f32080eff8bbcc4f8cd63c193efc0b41aafa0db
argeric <argeric@qq.com>
2026-07-07 14:50:10 +0800

    get logs by confura rpc

--- open-api/router/ESpaceApiRouter.ts
+++ open-api/router/ESpaceApiRouter.ts
@@ -631,7 +631,7 @@ async function listLogs(ctx) {
     topic3 && (topics[3] = topic3);
     const limit = 1000;
     const options = {fromBlock, toBlock, address, topics, limit};
-    const logArray = (await getApiService().eth?.getLogs(options)) || [];
+    const logArray = (await getApiService().confuraRpc?.getLogs(options)) || [];
     const result = logArray.slice(0, limit);
     setBody(ctx, result)
 }
