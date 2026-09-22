9d39af52896c939a25744f7f2908cb74eae0c58b
tigo9527 <tigo9527@no-reply.com>
2026-09-22 07:24:25 +0000

    fix(verify): normalize address and expose sourcify error payload

--- open-api/router/middleware.ts
+++ open-api/router/middleware.ts
@@ -46,7 +46,10 @@ export async function handleException(ctx, next) {
             setBody(ctx, undefined, CODE_RATE_LIMITED, err.toString())
             return
         }
-        setBody(ctx, undefined, 500, err.toString())
+        const remoteDetails = err?.remoteRespText
+            ? `${err.toString()}, remoteUrl: '${err?.remoteUrl || ''}', remoteRespText: '${err.remoteRespText}'`
+            : err.toString();
+        setBody(ctx, undefined, 500, remoteDetails)
         if (err.code == 500) {
             safeAddErrorLog('open', `open-500-${err.message}`, err);
         }
