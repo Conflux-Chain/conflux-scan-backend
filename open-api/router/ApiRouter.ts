7f7d4955f296363d2e6838a4f658106c58b372e3
tigo9527 <tigo9527@no-reply.com>
2026-09-22 06:08:51 +0000

    feat(core-openapi): add block epoch lookup by timestamp

--- open-api/router/ApiRouter.ts
+++ open-api/router/ApiRouter.ts
@@ -79,7 +79,13 @@ import {
     listCoreTransactionStat,
     listContractVerifiedStats,
 } from "../service/OpenStatService";
-import {checkPresent, mustBeAddressParamIfPresent,} from "../../stat/service/common/utils";
+import {closestEpochByTimeStamp, ClosestType} from "../../stat/model/Epoch";
+import {
+    checkPresent,
+    mustBeAddressParamIfPresent,
+    mustBeEnumParamIfPresent,
+    mustBeIntParamIfPresent,
+} from "../../stat/service/common/utils";
 import {
     checkApiKey,
     checkRateByAddress,
@@ -110,6 +116,22 @@ async function getTokenInfo(ctx) {
     }
 
     setBody(ctx, token)
+}
+
+async function getBlockNoByTime(ctx) {
+    mustBeIntParamIfPresent(ctx.request.query, 'timestamp');
+    mustBeEnumParamIfPresent(ctx.request.query, 'closest', ['before', 'after']);
+    const {timestamp, closest} = ctx.request.query;
+    checkPresent({timestamp}, ['timestamp']);
+
+    const closestType = (closest === 'before' || closest === undefined) ? ClosestType.BEFORE : ClosestType.AFTER
+    const epochNumber = await closestEpochByTimeStamp(closestType, timestamp)
+    if (!Number.isInteger(epochNumber)) {
+        setBody(ctx, undefined, 1, `blockno ${closest} timestamp ${timestamp} not found`)
+        return;
+    }
+
+    setBody(ctx, epochNumber)
 }
 
 export async function register(app: Koa, apiServer: ApiServer, port:string|number) {
@@ -188,6 +210,9 @@ function registerRouter(router: Router) {
     router.get('/contract/verified/latest', listVerifiedContractsLatest)
     router.get('/contract/lookupAbi', batchGetSignaturesByHashes)
     router.get('/contract/searchAbi', batchGetSignaturesByName)
+
+    // block
+    router.get('/block/getblocknobytime', getBlockNoByTime)
 
     // token
     router.get('/token/tokeninfo', getTokenInfo);
